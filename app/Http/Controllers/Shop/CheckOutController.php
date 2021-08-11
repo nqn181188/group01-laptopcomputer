6cc8025483518a6ac6ffc2160e854463ead41d50
HuanJiFeng <terminatau@gmail.com>
2021-08-11 17:10:24 +0700

    Hoang

--- app/Http/Controllers/Shop/CheckOutController.php
+++ app/Http/Controllers/Shop/CheckOutController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Customer;
+use App\Models\OrderDetail;
 class CheckOutController extends Controller
 {
     /**
@@ -83,5 +84,51 @@ class CheckOutController extends Controller
     public function destroy($id)
     {
         //
+    }
+    public function doCheckout(Request $request) {
+        $fname = $request->fname;
+        $lname = $request->lname;
+        $email = $request->email;
+        $phone = $request->phone;
+        $add = $request->add;
+        if ($request->session()->has('cart')) {
+            $cart = $request->session()->get('cart');
+            // tạo và lưu order
+            $ord = new Order();
+            $ord->firstname = $fname;
+            $ord->lastname = $lname;
+            $ord->email = $email;
+            $ord->phone = $phone;
+            $ord->address = $add;
+            $ord->order_date = \Carbon\Carbon::now();
+            // lưu order
+            $ord->save();
+
+            // xử lý order detail
+            foreach($cart as $item) {
+                $detail = new OrderDetail();
+                $detail->order_id = $ord->id;
+                $detail->product_id = $item->id;
+                $detail->quantity = $item->quantity;
+                $detail->price = $item->price;
+                $detail->save();
+            }
+        }
+
+        if (isset($request->createAccount)) {
+            $cust = new Customer();
+            $cust->firstname = $fname;
+            $cust->lastname = $lname;
+            $cust->username = $email;
+            $cust->password = \md5('123456');
+            $cust->email = $email;
+            $cust->phone = $phone;
+            $cust->address = $add;
+            $cust->save();
+        }
+
+        // xóa session
+        session()->forget('cart');
+        return redirect()->route('home');
     }
 }
