54b4ecd4e82749dbe4d175e0a3b2dd242d87f2cc
sluuthanh <sluuthanh@gmail.com>
2021-08-16 12:38:56 +0700

    Update check.blade.php

--- resources/views/admin/feedback/check.blade.php
+++ resources/views/admin/feedback/check.blade.php
@@ -37,66 +37,41 @@
         </div>
       </div>
       <div class="card-body p-0">
-        {{-- <form action="{{ route('admin.contact.update',  $feedback->id) }}" method="POST">
-          @method('put')
-          <input type="hidden" name="id" value="{{ $feedback->id}}">
-            @csrf
-
-            <label for="firstname">Firstname<span></span></label>
-            <input type="text" value="{{$feedback->firstname}}" id="firstname" name="firstname" readonly>
-
-            <label for="lastname">Lastname<span></span></label>
-            <input type="text" value="{{$feedback->lastname}}" id="lastname" name="lastname" readonly>
-
-            <label for="email">Email<span>*</span></label>
-            <input type="text" value="{{$feedback->email}}" id="email" name="email" readonly>
-
-            <label for="phone">Number Phone</label>
-            <input type="text" value="{{$feedback->phone}}" id="phone" name="phone" readonly>
-
-            <label for="comment">Comment</label>
-            <textarea name="comment" value="{{$feedback->comment}}" id="comment" readonly></textarea>
-
-            <label for="read">Read</label>
-            <input type="checkbox" id="read" name="read" 
-                    @if ($feedback->read)
-                        checked
-                    @endif>
-
-            <label for="note">Note</label>
-            <input type="text" id="note" name="note" 
-              value="@if (isset($feedback->note))
-                  {{$feedback->note}}
-              @endif">
-        </form> --}}
-        
-
         <form action="{{ route('admin.contact.update',  $feedback->id) }}" method="POST">
           @method('put')
           <input type="hidden" name="id" value="{{ $feedback->id}}">
           @csrf
-          <div class="form-group">
-              <label for="firstname">First name</label>
-              <input type="text" id="firstname" value="{{ old('firstname',$feedback->firstname)}}" name="firstname" class="form-control"/>
-          </div>
-          <div class="form-group">
-            <label for="lastname">Last name</label>
-            <input type="text" id="lastname" value="{{ old('lastname',$feedback->lastname)}}" name="lastname" class="form-control"/>
-          </div>
-         
-          <div class="form-group">
-              <label for="email">Email</label>
-              <input type="text" id="email" value="{{ old('email',$feedback->email)}}" name="email" class="form-control"/>
-          </div>
-          <div class="form-group">
-            <label for="address">Address</label>
-            <input type="text" id="address" value="{{ old('address',$feedback->comment)}}" name="address" class="form-control"/>
-          </div>
-          
-          <div class="form-group">
-              <input type="submit" name="btnUpdate" value="Update" class="btn btn-primary"/>
-          </div>
+
+          <label for="firstname">Firstname<span></span></label>
+          <input type="text" value="{{$feedback->firstname}}" id="firstname" name="firstname" readonly>
+
+          <label for="lastname">Lastname<span></span></label>
+          <input type="text" value="{{$feedback->lastname}}" id="lastname" name="lastname" readonly>
+
+          <label for="email">Email<span>*</span></label>
+          <input type="text" value="{{$feedback->email}}" id="email" name="email" readonly>
+
+          <label for="phone">Number Phone</label>
+          <input type="text" value="{{$feedback->phone}}" id="phone" name="phone" readonly>
+
+          <label for="comment">Comment</label>
+          <textarea name="comment" value="{{$feedback->comment}}" id="comment" readonly></textarea>
+
+          <label for="read">Read</label>
+          <input type="checkbox" id="read" name="read" 
+                  @if ($feedback->read)
+                      checked
+                  @endif>
+
+          <label for="note">Note</label>
+          <input type="text" id="note" name="note" 
+            value="@if (isset($feedback->note))
+                {{$feedback->note}}
+            @endif">
         </form>
+        
+
+       
       </div>
       <!-- /.card-body -->
     </div>
